6e54a66b19019a5cfadee12d64e5f08846027bc9
Krishnaburjangi720 <krishnaburjangi@gmail.com>
2026-02-17 11:15:07 +0530

    Create q4.py

---
+++ q4.py
@@ -0,0 +1,7 @@
+def get_ticket_price(age: int, is_student: bool) -> int:
+    if age < 12:
+        return 8
+    elif age >= 65:
+        return 10
+    else:
+        return 12 if is_student else 15
